7d10fea19675e028ffabe81e02471b10452334fb
d-kingangi <itsronduncan@gmail.com>
2023-12-15 12:53:01 +0300

    input

---
+++ input + 100.py
@@ -0,0 +1,18 @@
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old
+
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = 2023 - age + 100
+print(name + ", you will be 100 years old in the year " + str(year))
+
+# creating a function
+def hundredth_year(name, age):
+    year = 2023 - int(age) + 100
+    message = name + ", you will be a hundred years old in the year " + str(year)
+    return message
+
+name = input ("What is your name: ")  
+age = input("How old are you: ")
+message = hundredth_year(name, age)
+print(message) 
